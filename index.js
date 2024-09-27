872558e173197ca8413543bb83553979068dd931
phillybewillin <markphiloyaro27@gmail.com>
2024-09-27 13:28:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const tmdbApi = process.env.TMDB_KEY;
 const port = process.env.PORT;
 
 app.register(cors, { 
-    origin: 'http://localhost:5173',
+    origin: 'https://zilla-xr.xyz',
     methods: ['GET', 'POST'],
   })
 
